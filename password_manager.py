23eaedb66e8bf8dfb25c8f7c56a40bebf4ecfcc7
Al Gelders <agelders2021@earthlink.net>
2026-01-22 13:07:13 -0700

    Change in license commment only

--- password_manager.py
+++ password_manager.py
@@ -3,7 +3,7 @@ SPDX-License-Identifier: GPL-3.0-or-later
 
 Copyright (C) 2026 Al Gelders
 
-This file is part of the airscenting an trailing logging programs
+This file is part of the attendance logging programs
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
